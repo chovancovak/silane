ca1083d8c230cff59a509db0b627ecffeb5e18a0
Karolína Chovancová <karolina.chloubova@gmail.com>
2025-12-01 20:15:45 +0100

    homepage

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat `images/` do `_site/images`Pozor, zda nejsou img
+	eleventyConfig.addPassthroughCopy("img");
+
+	// Zkopírovat `css/` do `_site/css/`
+	eleventyConfig.addPassthroughCopy("css");
+ return {
+    // možné formáty šablon
+    templateFormats: ["njk","html", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
